9232d73054a4c8f7e90225d1045e34a2b7014138
Rivaldy <rivaldy@stimednp.ac.id>
2022-01-20 16:07:16 +0800

    :label: setup model & api providers

---
+++ lib/data/services/providers/movie_providers.dart
@@ -0,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:themoviedb/data/models/movie.dart';
+import 'package:themoviedb/data/models/movie_response.dart';
+import 'package:themoviedb/data/services/api_client.dart';
+import 'package:themoviedb/utils/util_constants.dart';
+
+class MovieProviders {
+  ApiClient _client;
+
+  MovieProviders(this._client);
+
+  Future<List<Movie>> fetchMovies() async {
+    try {
+      Response response = await _client.dio.get(
+        "discover/movie",
+        queryParameters: {"api_key": API_KEY},
+      );
+      return MovieResponse.fromJson(response.data).results!;
+    } on DioError catch (e) {
+      log("RVL_ERROR : ${e.message}");
+      return [];
+    }
+  }
+}
