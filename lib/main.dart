6b5ec7d7d57382379771a9d959e4a093acd960b7
Rivaldy <rivaldy@stimednp.ac.id>
2022-01-20 16:08:39 +0800

    :label: setup view (home page)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:themoviedb/app.dart';
+import 'package:themoviedb/data/repositories/movie_repository.dart';
+import 'package:themoviedb/data/services/api_client.dart';
+import 'package:themoviedb/data/services/providers/movie_providers.dart';
 
 void main() {
-  runApp(App());
+  runApp(App(
+    movieRepository: MovieRepository(MovieProviders(ApiClient())),
+  ));
 }
